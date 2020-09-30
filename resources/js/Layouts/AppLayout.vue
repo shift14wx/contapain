30100170ca37b6a7cfd4e57a54c066b913229872
Ac-04 <carloshernandez.isaias12.2@gmail.com>
2020-09-29 18:14:06 -0600

    :sparkles: quitanto todo el ruido de la pagina de landing

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -23,7 +23,7 @@
                     <!-- Settings Dropdown -->
                     <div class="hidden sm:flex sm:items-center sm:ml-6">
                         <div class="ml-3 relative">
-                            <jet-dropdown align="right" width="48">
+                            <jet-dropdown alignWelcome to your Jetstream application!="right" width="48">
                                 <template #trigger>
                                     <button v-if="$page.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
                                         <img class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
