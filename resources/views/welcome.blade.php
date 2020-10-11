80f1a1878312bffa51af6cb49746ba89e1db4e57
Ac-04 <carloshernandez.isaias12.2@gmail.com>
2020-10-10 20:15:29 -0600

    :sparkles: agregando descripcion de contapainless en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Tivo is a HTML landing page template built with Bootstrap to help you crate engaging presentations for SaaS apps and convert visitors into users.">
-    <meta name="author" content="Inovatik">
+    <meta name="description" content="ContapainLess T es el sistema que haran crecer tu productividad.">
+    <meta name="author" content="DiegosCorps">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
-    <meta property="og:site_name" content="" /> <!-- website name -->
-    <meta property="og:site" content="" /> <!-- website link -->
-    <meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
-    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+    <meta property="og:site_name" content="ContaPainLess T" /> <!-- website name -->
+    <meta property="og:site" content="https://tranquil-citadel-03171.herokuapp.com/" /> <!-- website link -->
+    <meta property="og:title" content="ContaPainLess"/> <!-- title shown in the actual shared post -->
+    <meta property="og:description" content="ContapainLess T es el sistema que haran crecer tu productividad." /> <!-- description shown in the actual shared post -->
     <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
     <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
     <meta property="og:type" content="article" />
